bc2597961f54d5b657a9198dccf44f0e3dc3de75
xPompunia <piotrratajczak111@gmail.com>
2024-04-06 16:27:33 +0200

    add constants.h

---
+++ constants.h
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+
+#define MAP_SIZE 16
+#define NR_OF_MINES 40
+
+#define WINDOW_WIDTH 512 // szerokość okna
+#define WINDOW_HEIGHT 512 // wysokość okna
+
+#endif
